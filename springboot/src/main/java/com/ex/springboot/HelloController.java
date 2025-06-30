17c89319c5ff853455c45d86b07e3f8c8e8b6852
syed Git <syed@git.com>
2025-06-30 15:43:12 +0530

    Triggering GitHub Actions

--- springboot/src/main/java/com/ex/springboot/HelloController.java
+++ springboot/src/main/java/com/ex/springboot/HelloController.java
@@ -3,7 +3,7 @@ package com.ex.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//triggering Github actions 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
